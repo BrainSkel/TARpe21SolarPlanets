0ca995435877b78a7ed0ed738fe0333b58966b39
BrainSkel <hanne53rik@gmail.com>
2023-03-22 16:05:33 +0200

    add planets page
    add planets page

--- SolarPlanets/SolarPlanets/Views/PlanetsPage.xaml.cs
+++ SolarPlanets/SolarPlanets/Views/PlanetsPage.xaml.cs
@@ -2,8 +2,22 @@ namespace SolarPlanets.Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	private const uint AnimationDuration = 800u;
 	public PlanetsPage()
 	{
 		InitializeComponent();
 	}
+
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+
+	private async Task CloseMenu()
+	{
+		//close emnu and show again main content
+		_ = MenuContainer.FadeTo(1, AnimationDuration);
+        _ = MenuContainer.ScaleTo(1, AnimationDuration);
+		await MenuContainer.TranslateTo(0, 0, AnimationDuration);
+    }
 }
